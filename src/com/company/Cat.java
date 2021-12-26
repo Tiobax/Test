b6a5afdf37703c2ee25538ce95aab425b6b80449
Buko Evgeny <Zederon@mail.ru>
2021-12-26 13:36:13 +0300

    add entity Cat

---
+++ src/com/company/Cat.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Cat {
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
